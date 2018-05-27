00bc414734fa27d3aac8272832aa1a2c60708a7e
mac <toshi.import@gmail.com>
2018-05-27 11:59:13 +0900

    edit itemscontroller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,12 @@ class ItemsController < ApplicationController
   end
 
   def pay
-
+    Payjp.api_key = 'sk_test_5856604ca5d22e70cf473d39'
+    Payjp::Charge.create(
+     #ここに金額(整数型)を代入する
+     :amount => 3500,
+     :card => params['payjp-token'],
+     :currency => 'jpy'
+    )
   end
 end
